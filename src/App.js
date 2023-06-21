530aac614dbdbafac0ef5f119e545e41a2b8d358
Jamshid Khaytbaev <itsjk0307@gmail.com>
2023-06-21 14:50:00 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from "react";
 import "./App.css";
-import ClickCounterTwo from "./Components/ClickCounterTwo";
-import HoverCounterTwo from "./Components/HoverCountTwo";
-// import User from "./Components/User";
-import CounterTwo from "./Components/CounterTwo";
+import HookCounterTree from "./Components/HookCounterTree";
+
 class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <CounterTwo
-          render={(count, incrementCount) => (
-            <ClickCounterTwo
-              count={count}
-              incrementCount={incrementCount}
-            ></ClickCounterTwo>
-          )}
-        />
-        <CounterTwo
-          render={(count, incrementCount) => (
-            <HoverCounterTwo
-              count={count}
-              incrementCount={incrementCount}
-            ></HoverCounterTwo>
-          )}
-        />
-
-        {/* <ClickCounterTwo />
-        <HoverCounterTwo />
-        <User render={(isLoggedIn) => (isLoggedIn ? "Jamshid" : "Guest")} /> */}
+        <HookCounterTree />
       </div>
     );
   }
